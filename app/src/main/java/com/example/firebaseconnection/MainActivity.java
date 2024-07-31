d935c649ce64a42585794ce53361b5460fb9f67d
GabrielCodexxx <shotishoti092403@gmail.com>
2024-07-31 12:32:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/firebaseconnection/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.firebaseconnection;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Apply system window insets to adjust padding
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize Firestore instance
+        firestore = FirebaseFirestore.getInstance();
+
+        // Create a user data map
+        Map<String, Object> ConnectTest = new HashMap<>();
+        ConnectTest.put("first", "Gabriel");
+        ConnectTest.put("last", "Portillo");
+        ConnectTest.put("age", 21);
+
+        // Add user data to "user" collection
+        firestore.collection("ConnectTest").add(ConnectTest)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
